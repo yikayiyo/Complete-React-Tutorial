48eb3d65c882db04ae907a4de3fd01b3e4a462ed
yikayiyo <837937787@qq.com>
2021-04-07 09:55:06 +0800

    抽出端口，删除多于代码

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,17 +1,19 @@
-import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 import useFetch from "./useFetch";
 
 const Home = () => {
-  const { error, isPending, data: blogs } = useFetch('http://localhost:8000/blogs')
+  const PORT = 5000;
+  const { error, isPending, data: blogs } = useFetch(
+    `http://localhost:${PORT}/blogs`
+  );
 
   return (
     <div className="home">
-      { error && <div>{ error }</div> }
-      { isPending && <div>Loading...</div> }
-      { blogs && <BlogList blogs={blogs} /> }
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} />}
     </div>
   );
-}
- 
+};
+
 export default Home;
